582dae038acf2079791e23e8171cacbeb04a2388
lenpino <lenpino@gmail.com>
2013-04-30 17:05:46 -0400

    - Implementacion del DAO para la tabla de la informacion de programa

--- accesodb/src/cl/altair/acceso/dao/ProgramaDAO.java
+++ accesodb/src/cl/altair/acceso/dao/ProgramaDAO.java
@@ -1,270 +1,195 @@
 package cl.altair.acceso.dao;
-
 import java.util.List;
 import java.util.logging.Level;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+import cl.altair.acceso.modelo.Programa;
 
-import cl.altair.acceso.modelo.Persona;
 
 /**
- * A data access object (DAO) providing persistence and search support for
- * Persona entities. Transaction control of the save(), update() and delete()
- * operations must be handled externally by senders of these methods or must be
- * manually added to each of these methods for data to be persisted to the JPA
- * datastore.
- * 
- * @see cl.mycompany.acceso.modelo.Persona
- * @author MyEclipse Persistence Tools
- */
+ 	* A data access object (DAO) providing persistence and search support for Empresa entities.
+ 	* Transaction control of the save(), update() and delete() operations must be handled externally by senders of these methods or must be manually added to each of these methods for data to be persisted to the JPA datastore.	
+ 	* @see com.acepta.perfilamiento.eclipselink.Empresa
+  * @author MyEclipse Persistence Tools 
+  * */
 
-public class ProgramaDAO {
-	// property constants
-	public static final String RUT = "rut";
-	public static final String DV = "dv";
-	public static final String NOMBRE = "nombre";
-	public static final String APELLIDO = "apellido";
-	public static final String NOMBREAD = "nombread";
-	public static final String APELLIDOMAT = "apellidomat";
-	public static final String FECHANAC = "fechanac";
-	public static final String EMAIL = "email";
-	public static final String CELULAR = "celular";
+public class ProgramaDAO  {
+	//property constants
+	public static final String ACTIVACION = "activacion";
+	public static final String SERIAL = "serial";
+	public static final String ESTADO = "estado";
+	public static final String VERSION = "version";
 
-	private EntityManager getEntityManager() {
+
+
+
+	private static EntityManager getEntityManager() {
 		return EntityManagerHelper.getEntityManager();
-	}
-
-	/**
-	 * Perform an initial save of a previously unsaved Persona entity. All
-	 * subsequent persist actions of this entity should use the #update()
-	 * method. This operation must be performed within the a database
-	 * transaction context for the entity's data to be permanently saved to the
-	 * persistence store, i.e., database. This method uses the
-	 * {@link javax.persistence.EntityManager#persist(Object)
-	 * EntityManager#persist} operation.
-	 * 
-	 * <pre>
-	 * EntityManagerHelper.beginTransaction();
-	 * PersonaDAO.save(entity);
-	 * EntityManagerHelper.commit();
+	}	
+	
+		/**
+	 Perform an initial save of a previously unsaved Programa entity. 
+	 All subsequent persist actions of this entity should use the #update() method.
+	 This operation must be performed within the a database transaction context for the entity's data to be permanently saved to the persistence store, i.e., database. 
+	 This method uses the {@link javax.persistence.EntityManager#persist(Object) EntityManager#persist} operation.
+	 	 
+	 * <pre> 
+	 *   EntityManagerHelper.beginTransaction();
+	 *   ProgramaDAO.save(entity);
+	 *   EntityManagerHelper.commit();
 	 * </pre>
-	 * 
-	 * @param entity
-	 *            Persona entity to persist
-	 * @throws RuntimeException
-	 *             when the operation fails
+	   @param entity Programa entity to persist
+	  @throws RuntimeException when the operation fails
 	 */
-	public void save(Persona entity) {
-		EntityManagerHelper.log("saving Persona instance", Level.INFO, null);
-		try {
-			getEntityManager().persist(entity);
-			EntityManagerHelper.log("save successful", Level.INFO, null);
-		} catch (RuntimeException re) {
-			EntityManagerHelper.log("save failed", Level.SEVERE, re);
-			throw re;
-		}
-	}
-
-	/**
-	 * Delete a persistent Persona entity. This operation must be performed
-	 * within the a database transaction context for the entity's data to be
-	 * permanently deleted from the persistence store, i.e., database. This
-	 * method uses the {@link javax.persistence.EntityManager#remove(Object)
-	 * EntityManager#delete} operation.
-	 * 
+    public void save(cl.altair.acceso.modelo.Programa entity) {
+    		EntityManagerHelper.log("saving Programa instance", Level.INFO, null);
+	        try {
+	        	getEntityManager().persist(entity);
+            	EntityManagerHelper.log("save successful", Level.INFO, null);
+	        } catch (RuntimeException re) {
+	        	EntityManagerHelper.closeEntityManager();
+        		EntityManagerHelper.log("save failed", Level.SEVERE, re);
+	            throw re;
+        }
+    }
+    
+    /**
+	 Delete a persistent Programa entity.
+	  This operation must be performed 
+	 within the a database transaction context for the entity's data to be
+	 permanently deleted from the persistence store, i.e., database. 
+	 This method uses the {@link javax.persistence.EntityManager#remove(Object) EntityManager#delete} operation.
+	 	  
 	 * <pre>
-	 * EntityManagerHelper.beginTransaction();
-	 * PersonaDAO.delete(entity);
-	 * EntityManagerHelper.commit();
-	 * entity = null;
+	 *   EntityManagerHelper.beginTransaction();
+	 *   ProgramaDAO.delete(entity);
+	 *   EntityManagerHelper.commit();
+	 *   entity = null;
 	 * </pre>
-	 * 
-	 * @param entity
-	 *            Persona entity to delete
-	 * @throws RuntimeException
-	 *             when the operation fails
+	   @param entity Programa entity to delete
+	 @throws RuntimeException when the operation fails
 	 */
-	public void delete(Persona entity) {
-		EntityManagerHelper.log("deleting Persona instance", Level.INFO, null);
-		try {
-			entity = getEntityManager().getReference(Persona.class,
-					entity.getId());
-			getEntityManager().remove(entity);
-			EntityManagerHelper.log("delete successful", Level.INFO, null);
-		} catch (RuntimeException re) {
-			EntityManagerHelper.log("delete failed", Level.SEVERE, re);
-			throw re;
-		}
-	}
-
-	/**
-	 * Persist a previously saved Persona entity and return it or a copy of it
-	 * to the sender. A copy of the Persona entity parameter is returned when
-	 * the JPA persistence mechanism has not previously been tracking the
-	 * updated entity. This operation must be performed within the a database
-	 * transaction context for the entity's data to be permanently saved to the
-	 * persistence store, i.e., database. This method uses the
-	 * {@link javax.persistence.EntityManager#merge(Object) EntityManager#merge}
-	 * operation.
-	 * 
+    public void delete(Programa entity) {
+    				EntityManagerHelper.log("deleting Programa instance", Level.INFO, null);
+	        try {
+        	entity = getEntityManager().getReference(Programa.class, entity.getId());
+            getEntityManager().remove(entity);
+            			EntityManagerHelper.log("delete successful", Level.INFO, null);
+	        } catch (RuntimeException re) {
+	        	EntityManagerHelper.closeEntityManager();
+        		EntityManagerHelper.log("delete failed", Level.SEVERE, re);
+	            throw re;
+        }
+    }
+    
+    /**
+	 Persist a previously saved Programa entity and return it or a copy of it to the sender. 
+	 A copy of the Programa entity parameter is returned when the JPA persistence mechanism has not previously been tracking the updated entity. 
+	 This operation must be performed within the a database transaction context for the entity's data to be permanently saved to the persistence
+	 store, i.e., database. This method uses the {@link javax.persistence.EntityManager#merge(Object) EntityManager#merge} operation.
+	 	 
 	 * <pre>
-	 * EntityManagerHelper.beginTransaction();
-	 * entity = PersonaDAO.update(entity);
-	 * EntityManagerHelper.commit();
+	 *   EntityManagerHelper.beginTransaction();
+	 *   entity = ProgramaDAO.update(entity);
+	 *   EntityManagerHelper.commit();
 	 * </pre>
-	 * 
-	 * @param entity
-	 *            Persona entity to update
-	 * @return Persona the persisted Persona entity instance, may not be the
-	 *         same
-	 * @throws RuntimeException
-	 *             if the operation fails
+	   @param entity Programa entity to update
+	 @return Programa the persisted Programa entity instance, may not be the same
+	 @throws RuntimeException if the operation fails
 	 */
-	public Persona update(Persona entity) {
-		EntityManagerHelper.log("updating Persona instance", Level.INFO, null);
-		try {
-			Persona result = getEntityManager().merge(entity);
-			EntityManagerHelper.log("update successful", Level.INFO, null);
-			return result;
-		} catch (RuntimeException re) {
-			EntityManagerHelper.log("update failed", Level.SEVERE, re);
-			throw re;
-		}
-	}
+    public Programa update(Programa entity) {
+    				EntityManagerHelper.log("updating Programa instance", Level.INFO, null);
+	        try {
+            Programa result = getEntityManager().merge(entity);
+            			EntityManagerHelper.log("update successful", Level.INFO, null);
+	            return result;
+        } catch (RuntimeException re) {
+        	EntityManagerHelper.closeEntityManager();
+        				EntityManagerHelper.log("update failed", Level.SEVERE, re);
+	            throw re;
+        }
+    }
+    
+    public Programa findById( Integer id) {
+    				EntityManagerHelper.log("finding Programa instance with id: " + id, Level.INFO, null);
+	        try {
+            Programa instance = getEntityManager().find(Programa.class, id);
+            return instance;
+        } catch (RuntimeException re) {
+        	EntityManagerHelper.closeEntityManager();
+        		EntityManagerHelper.log("find failed", Level.SEVERE, re);
+	            throw re;
+        } finally{
+        	EntityManagerHelper.closeEntityManager();
+        }
+    }    
+    
 
-	public Persona findById(Integer id) {
-		EntityManagerHelper.log("finding Persona instance with id: " + id,
-				Level.INFO, null);
-		try {
-			Persona instance = getEntityManager().find(Persona.class, id);
-			return instance;
+/**
+	 * Find all Programa entities with a specific property value.  
+	 
+	  @param propertyName the name of the Programa property to query
+	  @param value the property value to match
+	  	  @return List<Programa> found by query
+	 */
+    @SuppressWarnings("unchecked")
+    public List<Programa> findByProperty(String propertyName, final Object value
+        ) {
+    				EntityManagerHelper.log("finding Programa instance with property: " + propertyName + ", value: " + value, Level.INFO, null);
+			try {
+			final String queryString = "select model from Programa model where model." 
+			 						+ propertyName + "= :propertyValue";
+								Query query = getEntityManager().createQuery(queryString);
+					query.setParameter("propertyValue", value);
+					return query.getResultList();
 		} catch (RuntimeException re) {
-			EntityManagerHelper.log("find failed", Level.SEVERE, re);
-			throw re;
+			EntityManagerHelper.closeEntityManager();
+						EntityManagerHelper.log("find by property name failed", Level.SEVERE, re);
+				throw re;
+		} finally{
+			EntityManagerHelper.closeEntityManager();
 		}
+	}			
+	public List<Programa> findByActivacion(Object nombre
+	) {
+		return findByProperty(ACTIVACION, nombre
+		);
+	}
+	
+	public List<Programa> findBySerial(Object rut
+	) {
+		return findByProperty(SERIAL, rut
+		);
+	}
+	
+	public List<Programa> findByVersion(Object telefono
+	) {
+		return findByProperty(VERSION, telefono
+		);
+	}
+	
+	public List<Programa> findByEstado(Object estado) {
+		return findByProperty(ESTADO, estado);
 	}
 
 	/**
-	 * Find all Persona entities with a specific property value.
-	 * 
-	 * @param propertyName
-	 *            the name of the Persona property to query
-	 * @param value
-	 *            the property value to match
-	 * @param rowStartIdxAndCount
-	 *            Optional int varargs. rowStartIdxAndCount[0] specifies the the
-	 *            row index in the query result-set to begin collecting the
-	 *            results. rowStartIdxAndCount[1] specifies the the maximum
-	 *            number of results to return.
-	 * @return List<Persona> found by query
+	 * Find all Programa entities.
+	  	  @return List<Programa> all Programa entities
 	 */
 	@SuppressWarnings("unchecked")
-	public List<Persona> findByProperty(String propertyName,
-			final Object value, final int... rowStartIdxAndCount) {
-		EntityManagerHelper.log("finding Persona instance with property: "
-				+ propertyName + ", value: " + value, Level.INFO, null);
-		try {
-			final String queryString = "select model from Persona model where model."
-					+ propertyName + "= :propertyValue";
-			Query query = getEntityManager().createQuery(queryString);
-			query.setParameter("propertyValue", value);
-			if (rowStartIdxAndCount != null && rowStartIdxAndCount.length > 0) {
-				int rowStartIdx = Math.max(0, rowStartIdxAndCount[0]);
-				if (rowStartIdx > 0) {
-					query.setFirstResult(rowStartIdx);
-				}
-
-				if (rowStartIdxAndCount.length > 1) {
-					int rowCount = Math.max(0, rowStartIdxAndCount[1]);
-					if (rowCount > 0) {
-						query.setMaxResults(rowCount);
-					}
-				}
-			}
-			return query.getResultList();
+	public List<Programa> findAll(
+		) {
+					EntityManagerHelper.log("finding all Programa instances", Level.INFO, null);
+			try {
+			final String queryString = "select model from Programa model order by model.activacion";
+								Query query = getEntityManager().createQuery(queryString);
+					return query.getResultList();
 		} catch (RuntimeException re) {
-			EntityManagerHelper.log("find by property name failed",
-					Level.SEVERE, re);
-			throw re;
+			EntityManagerHelper.closeEntityManager();
+						EntityManagerHelper.log("find all failed", Level.SEVERE, re);
+				throw re;
+		} finally{
+			EntityManagerHelper.closeEntityManager();
 		}
 	}
-
-	public List<Persona> findByRut(Object rut, int... rowStartIdxAndCount) {
-		return findByProperty(RUT, rut, rowStartIdxAndCount);
-	}
-
-	public List<Persona> findByDv(Object dv, int... rowStartIdxAndCount) {
-		return findByProperty(DV, dv, rowStartIdxAndCount);
-	}
-
-	public List<Persona> findByNombre(Object nombre, int... rowStartIdxAndCount) {
-		return findByProperty(NOMBRE, nombre, rowStartIdxAndCount);
-	}
-
-	public List<Persona> findByApellido(Object apellido,
-			int... rowStartIdxAndCount) {
-		return findByProperty(APELLIDO, apellido, rowStartIdxAndCount);
-	}
-
-	public List<Persona> findByNombread(Object nombread,
-			int... rowStartIdxAndCount) {
-		return findByProperty(NOMBREAD, nombread, rowStartIdxAndCount);
-	}
-
-	public List<Persona> findByApellidomat(Object apellidomat,
-			int... rowStartIdxAndCount) {
-		return findByProperty(APELLIDOMAT, apellidomat, rowStartIdxAndCount);
-	}
-
-	public List<Persona> findByFechanac(Object fechanac,
-			int... rowStartIdxAndCount) {
-		return findByProperty(FECHANAC, fechanac, rowStartIdxAndCount);
-	}
-
-	public List<Persona> findByEmail(Object email, int... rowStartIdxAndCount) {
-		return findByProperty(EMAIL, email, rowStartIdxAndCount);
-	}
-
-	public List<Persona> findByCelular(Object celular,
-			int... rowStartIdxAndCount) {
-		return findByProperty(CELULAR, celular, rowStartIdxAndCount);
-	}
-
-	/**
-	 * Find all Persona entities.
-	 * 
-	 * @param rowStartIdxAndCount
-	 *            Optional int varargs. rowStartIdxAndCount[0] specifies the the
-	 *            row index in the query result-set to begin collecting the
-	 *            results. rowStartIdxAndCount[1] specifies the the maximum
-	 *            count of results to return.
-	 * @return List<Persona> all Persona entities
-	 */
-	@SuppressWarnings("unchecked")
-	public List<Persona> findAll(final int... rowStartIdxAndCount) {
-		EntityManagerHelper.log("finding all Persona instances", Level.INFO,
-				null);
-		try {
-			final String queryString = "select model from Persona model";
-			Query query = getEntityManager().createQuery(queryString);
-			if (rowStartIdxAndCount != null && rowStartIdxAndCount.length > 0) {
-				int rowStartIdx = Math.max(0, rowStartIdxAndCount[0]);
-				if (rowStartIdx > 0) {
-					query.setFirstResult(rowStartIdx);
-				}
-
-				if (rowStartIdxAndCount.length > 1) {
-					int rowCount = Math.max(0, rowStartIdxAndCount[1]);
-					if (rowCount > 0) {
-						query.setMaxResults(rowCount);
-					}
-				}
-			}
-			return query.getResultList();
-		} catch (RuntimeException re) {
-			EntityManagerHelper.log("find all failed", Level.SEVERE, re);
-			throw re;
-		}
-	}
-
 }
